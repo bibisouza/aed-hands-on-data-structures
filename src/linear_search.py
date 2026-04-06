c5c0e4c3757c7100ad1fce30aaa543865ee5bb0b
Beatriz <biabiasouza17@gmail.com>
2026-04-06 20:41:37 -0300

    Busca LInear

--- src/linear_search.py
+++ src/linear_search.py
@@ -8,4 +8,7 @@ def linear_search(array: MyArray, target: int) -> int:
     Deve retornar o índice da primeira ocorrência do valor,
     ou -1 caso o valor não esteja presente.
     """
-    raise NotImplementedError
+    for index in range(len(array)):
+        if array[index] == target:
+            return index
+    return -1
