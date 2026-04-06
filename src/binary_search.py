39e99d90d1a375ef45f90890f05031e5123c8b99
Beatriz <biabiasouza17@gmail.com>
2026-04-06 20:49:28 -0300

    Busca binaria

--- src/binary_search.py
+++ src/binary_search.py
@@ -7,4 +7,17 @@ def binary_search(array: MyArray, target: int) -> int:
 
     Deve retornar o índice do elemento ou -1 caso não encontrado.
     """
-    raise NotImplementedError
+    right = len(array) - 1
+    left = 0
+
+    while left <= right:
+        med = (left + right) // 2
+        med_val = array[med]
+
+        if med_val == target:
+            return med
+        elif med_val < target:
+            left = med + 1
+        else:
+            right = med - 1
+    return -1
